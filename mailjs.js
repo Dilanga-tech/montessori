548d3e5ab7db5eabfb5e7ff566d71328a7733b0d
Thennakoon-Git <80156612+Dilanga-tech@users.noreply.github.com>
2025-04-05 22:47:28 +0530

    update

--- mailjs.js
+++ mailjs.js
@@ -13,7 +13,7 @@
       `Name: ${name}\nEmail: ${email}\nDate of Birth: ${birth}\nStart Date: ${start}\nPhone: ${phone}\nMessage:\n${message}`
     );
 
-    const mailtoLink = `mailto:thennakoondilanga@gmail.com.com?subject=${subject}&body=${body}`;
+    const mailtoLink = `mailto:thennakoondilanga@gmail.com?subject=${subject}&body=${body}`;
 
     window.location.href = mailtoLink;
     return false; // prevent form submission
